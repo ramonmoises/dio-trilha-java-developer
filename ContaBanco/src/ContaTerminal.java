2317e19660ae541788c46fdc9f73e081880b9c2a
ramonmoises <ramonmoises@gmail.com>
2023-04-25 22:30:42 -0300

    Entrega do projeto Conta Bancária.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      
+        
+        Scanner entradaAgencia = new Scanner(System.in);
+        System.out.println("Por favor, digite o número da Agência!");
+        String Agencia = entradaAgencia.nextLine();
+
+        Scanner entradaNumero = new Scanner(System.in);
+        System.out.println("Por favor, digite o número da conta!");
+        int Numero = Integer.parseInt(entradaNumero.nextLine());
+
+        Scanner entradaNomeCliente = new Scanner(System.in);
+        System.out.println("Por favor, digite seu nome!");
+        String NomeCliente = entradaNomeCliente.nextLine();
+
+        Scanner entradaSaldo = new Scanner(System.in);
+        System.out.println("Por favor, digite o saldo da conta!");
+        Double Saldo = Double.parseDouble(entradaSaldo.nextLine());
+
+        System.out.println("Olá " + NomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + Agencia + ", conta " + Numero + " e seu saldo R$067-8" + Saldo + " já está disponível para saque." );
+
+        
+
     }
 }
